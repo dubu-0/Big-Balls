599fd02171359f1e021b519d1204c34a7d41e86d
dubu-0 <yudin01@mail.ru>
2021-11-09 23:01:06 +0300

    Кнопка рестарт теперь рабочая, другой баг - после рестарта пустой пул объектов

--- Assets/RestartButton.cs
+++ Assets/RestartButton.cs
@@ -7,18 +7,26 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class RestartButton : MonoBehaviour
 {
-    private Action _activateGameObject;
+    [SerializeField] private BoxCollider2D cover;
+    
+    private Action _showButton;
     
     private void Awake()
     {
-        _activateGameObject = () => gameObject.SetActive(true);
-        HealthPointsModel.Instance.OnDied += _activateGameObject;
+        _showButton = () => gameObject.SetActive(true);
+        HealthPointsModel.Instance.OnDied += _showButton;
+        HealthPointsModel.Instance.OnDied += DisallowUserClicks;
+        cover.enabled = false;
+        gameObject.SetActive(false);
     }
 
     private void OnDestroy()
     {
-        HealthPointsModel.Instance.OnDied -= _activateGameObject;
+        HealthPointsModel.Instance.OnDied -= _showButton;
+        HealthPointsModel.Instance.OnDied -= DisallowUserClicks;
     }
 
+    private void DisallowUserClicks() => cover.enabled = true;
+    
     public void RestartCurrentSceneOnClick() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 }
